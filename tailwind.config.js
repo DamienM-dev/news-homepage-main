2a5b1b86705a301d21542e1e18b4b76491650605
Damien MIREMONT <damien.miremont@gmail.com>
2023-09-16 16:07:54 +0200

    add pics and style (fonts, margin)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['inter', 'sans-serif']
+      },
+      colors: {
+        'soft-orange': 'hsl(35, 77%, 62%)',
+        'soft-red': 'hsl(5, 85%, 63%)',
+        'off-white': 'hsl(36, 100%, 99%)',
+        'Grayish-blue': 'hsl(233, 8%, 79%)',
+        'Dark-grayish-blue': 'hsl(236, 13%, 42%)',
+        'Very-dark-blue': 'hsl(240, 100%, 5%)'
+      }
+    },
   },
   plugins: [],
 }
+
+
+
+
+
+
+
+
